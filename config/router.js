881fb6e9ad74a0710f4cfd878432de7330fa9ecb
albertvinicius100 <117119890+albertvinicius100@users.noreply.github.com>
2023-02-23 02:34:58 -0400

    Create router.js

---
+++ config/router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mainController = require("../app/controllers/main");
+const areaController = require("../app/controllers/area");
+const router = express.Router();
+
+router.get("/"      , mainController.index);
+router.get("/sobre" , mainController.sobre);
+router.get("/ui"    , mainController.ui);
+
+router.get("/area"  , areaController.index);
+
+module.exports = router;
