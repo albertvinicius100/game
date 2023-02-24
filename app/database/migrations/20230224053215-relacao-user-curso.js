cba4a09e101b256fd93a83817cbd41565c360742
albertvinicius100 <117119890+albertvinicius100@users.noreply.github.com>
2023-02-24 03:01:45 -0400

    Add files via upload

---
+++ app/database/migrations/20230224053215-relacao-user-curso.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addConstraint('Users', {
+    type: 'foreign key',
+    fields: ['cursoId'],
+    name: 'users_curso_fk',
+    references: {
+      table: 'Cursos',
+      field: 'id'
+    },
+    onDelete: 'restrict',
+    onUpdate: 'restrict'
+    })
+  },
+
+  down: async(queryInterface, Sequelize) =>{
+    await queryInterface.removeConstraint(
+      'Users',
+      'users_curso_fk'
+    )
+  }
+};
