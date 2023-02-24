6c674e2d64b799d9b8dc21fb0bf0d2822c379c94
albertvinicius100 <117119890+albertvinicius100@users.noreply.github.com>
2023-02-24 03:05:34 -0400

    Update router.js

--- config/router.js
+++ config/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mainController = require("../app/controllers/main");
 const areaController = require("../app/controllers/area");
 const cursoController = require("../app/controllers/curso");
+const gameController = require("../app/controllers/game");
 const router = express.Router();
 
 router.get("/"      , mainController.index);
@@ -10,11 +11,16 @@ router.get("/ui"    , mainController.ui);
 
 router.get("/area"  , areaController.index);
 
+router.get("/game"  , gameController.index);
+router.get("/game_over"  , gameController.over);
 
-router.get("/curso"           , cursoController.index);
-router.get("/curso/create"    , cursoController.create);
+router.get("/curso"            , cursoController.index);
+router.get("/curso/create"     , cursoController.create);
 router.post("/curso/create"    , cursoController.create);
-router.get("/curso/:id"    , cursoController.read);
+router.get("/curso/update/:id"     , cursoController.update);
+router.post("/curso/update/:id"    , cursoController.update);
+router.get("/curso/remove/:id"        , cursoController.remove);
+router.get("/curso/:id"        , cursoController.read);
 
 
 module.exports = router;
