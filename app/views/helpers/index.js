b3fb399928410a541b45b4f2f3c44cf864ce29eb
albertvinicius100 <117119890+albertvinicius100@users.noreply.github.com>
2023-02-24 03:04:25 -0400

    Update index.js

--- app/views/helpers/index.js
+++ app/views/helpers/index.js
@@ -4,4 +4,26 @@ function toLower(str){
 function toUpper(str){
     return str.toUpperCase();
 }
-module.exports = { toLower, toUpper }
+
+function checked(currentValue, value){
+    if(currentValue == value){
+        return "checked";
+    }else {
+        return "";
+    }
+}
+
+const printError = function (errors, field) {
+    let mensagem;
+    if (typeof errors != 'undefined') {
+    errors.errors.forEach(error=> {
+    if (error.path == field) {
+        mensagem = error.message;
+        return;
+    }
+    });
+    return mensagem;
+    }
+    }
+
+module.exports = { toLower, toUpper, checked, printError}
